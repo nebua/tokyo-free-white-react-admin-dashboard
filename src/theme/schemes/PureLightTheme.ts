166a7346499989ca47045d76149e0bc73797f2a9
Angelo Correia <angelo.correia@devgo.org>
2023-11-15 10:37:42 -0100

    chore: customize theme

--- src/theme/schemes/PureLightTheme.ts
+++ src/theme/schemes/PureLightTheme.ts
@@ -2,7 +2,7 @@ import { alpha, createTheme, lighten, darken } from '@mui/material';
 import '@mui/lab/themeAugmentation';
 
 const themeColors = {
-  primary: '#5569ff',
+  primary: '#95C11F',
   secondary: '#6E759F',
   success: '#57CA22',
   warning: '#FFA319',
